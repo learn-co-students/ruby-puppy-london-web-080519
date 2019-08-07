a14a2ceeaa0b6385b9743473e95ebb67dcbcbe23
99chris99 <chrishodge87@gmail.com>
2019-08-07 10:22:49 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
-# Add your code here
+#require "pry"
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def save
+        @@all << self
+
+    end    
+
+    def initialize(name)
+        @name = name
+        #@@all << self
+        save
+    end
+        
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        self.all.each{|dog| puts "#{dog.name}"}
+        #binding.pry
+        #0
+    end
+
+end
+
+
+
+Dog.new("Rex")
+Dog.print_all

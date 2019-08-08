d2b6dfcf54b4f0d3753abf7520bc6e2e5bf9fa25
tegansims <github email address>
2019-08-08 14:33:04 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+
+require "pry"
+
+class Dog 
+    attr_accessor :name
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        # @@all << self
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        Dog.all.select { | dog | puts dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+end
